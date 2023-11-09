2f1cdcc4a46ac1533d0d1a981278f6810dd0ed69
Md Shawon Reza <fahadshawon241@gmail.com>
2023-11-09 19:05:33 +0600

    add this put method for update jobs

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.get('/', (req, res) => {
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.aqbtto6.mongodb.net/?retryWrites=true&w=majority`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// Create a MongoClient with a MongoClientOptions 
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
